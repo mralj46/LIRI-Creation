ce0668ff614525f05dcdbc4d835fde26f49d13ef
mralj46 <alj_thebest@yahoo.com>
2018-04-21 09:58:02 -0400

    update

---
+++ key.js
@@ -0,0 +1,9 @@
+// console.log('this is loaded');
+
+exports.twitterKeys = {
+    consumer_key: 'iCiszZ49O2gJmRMq8ozralsx4',
+    consumer_secret: 'v6PWgN4kWrdATahnt6nTObH7S4qUj7UInuH0eZ3grMnac0AHyJ',
+    access_token_key: '948624061835431937-qm4HPgrDhb9seDXOPLUDdfXdvDDi3Kc',
+    access_token_secret: 'qXfpTAqDiCsEJ6gUBcwYvFvQdENTJB6Md1SuWBfBsN6oU',
+
+  }
